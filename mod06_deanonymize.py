775b043a80d8904d897444a5c93bd2070b8b81b6
Parth Sachdev <parth.sachdev@uconn.edu>
2026-03-09 22:11:33 -0400

    finished mod06 deanonymization assignment

--- mod06_deanonymize.py
+++ mod06_deanonymize.py
@@ -18,7 +18,31 @@ def link_records(anon_df, aux_df):
       anon_id, matched_name
     containing ONLY uniquely matched records.
     """
-    raise NotImplementedError
+    rows = []
+
+    for i in range(len(anon_df)):
+        a_age = anon_df.loc[i, "age"]
+        a_zip = anon_df.loc[i, "zip3"]
+        a_gender = anon_df.loc[i, "gender"]
+
+        count = 0
+        match_name = ""
+
+        for j in range(len(aux_df)):
+            b_age = aux_df.loc[j, "age"]
+            b_zip = aux_df.loc[j, "zip3"]
+            b_gender = aux_df.loc[j, "gender"]
+
+            if a_age == b_age and a_zip == b_zip and a_gender == b_gender:
+                count = count + 1
+                match_name = aux_df.loc[j, "name"]
+
+        if count == 1:
+            rows.append([anon_df.loc[i, "anon_id"], match_name])
+
+    out = pd.DataFrame(rows, columns=["anon_id", "matched_name"])
+    return out
+    
 
 
 def deanonymization_rate(matches_df, anon_df):
@@ -26,4 +50,6 @@ def deanonymization_rate(matches_df, anon_df):
     Compute the fraction of anonymized records
     that were uniquely re-identified.
     """
-    raise NotImplementedError
+    
+    rate = len(matches_df) / len(anon_df)
+    return rate
